3c4a3b12bcb5f4067348f1a07f77fad836174156
Ignacio Rivadero <rivaderonacho@gmail.com>
2025-11-19 15:26:58 -0300

    feat: email column size is now 255 bytes

--- include/row.h
+++ include/row.h
@@ -8,12 +8,12 @@
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
 #define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
 
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 #define ID_SIZE (size_of_attribute(Row, id))
